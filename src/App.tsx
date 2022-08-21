f02ae39ae24b64c3871f4e0366b32c93c968c78a
enviel97 <nguyenvinhloc.07.10.1997@gmail.com>
2022-08-22 01:52:08 +0700

    bowl animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import CubeRotate from "./components/CubeRotate";
 import NeonCircle from "./components/NeonCircle";
 import NeonSquare from "./components/NeonSquare";
+import RoundBottomFlasks from "./components/RoundBottomFlasks";
 
 function App() {
   return (
     <div className='App'>
+      <RoundBottomFlasks />
       <CubeRotate />
       <NeonSquare />
       <NeonCircle />
---
+++ src/components/RoundBottomFlasks/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./RoundBottomFlasks.scss";
+
+const RoundBottomFlasks = () => {
+  return (
+    <section className='center'>
+      <div className='container center'>
+        <div className='shadow' />
+        <div className='bowl'>
+          <div className='liquid'></div>
+        </div>
+        <div className='star'></div>
+      </div>
+    </section>
+  );
+};
+
+export default RoundBottomFlasks;
