d45afaa20523e2d11e86dc816d0f700d4fbafafb
enviel97 <nguyenvinhloc.07.10.1997@gmail.com>
2022-08-25 17:04:51 +0700

    button decorate

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import CubeRotate from "./components/CubeRotate";
-import NeonCircle from "./components/NeonCircle";
-import NeonSquare from "./components/NeonSquare";
-import RoundBottomFlasks from "./components/RoundBottomFlasks";
+import ButtonToggle from "./components/Button/ButtonToggle";
 
 function App() {
   return (
     <div className='App'>
-      <RoundBottomFlasks />
-      <CubeRotate />
-      <NeonSquare />
-      <NeonCircle />
+      <ButtonToggle />
     </div>
   );
 }
---
+++ src/components/Animation/index.ts
@@ -0,0 +1,4 @@
+export { default as CubeRotate } from "./CubeRotate";
+export { default as NeonSquare } from "./NeonSquare";
+export { default as NeonCircle } from "./NeonCircle";
+export { default as RoundBottomFlasks } from "./RoundBottomFlasks";
